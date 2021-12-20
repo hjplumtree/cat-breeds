4f385965b94bece8f2b508452c9bc3537a841a54
hej <dark199174@gmail.com>
2021-12-20 20:45:40 +0900

    Add get more cat info

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ function getCatsBreedAndPhoto() {
   return fetch("https://api.thecatapi.com/v1/breeds")
     .then((res) => res.json())
     .then((data) =>
-      data.filter((cat) => cat.image).map((cat) => [cat.id, cat.image.url]),
+      data.filter((cat) => {
+        if (cat.image) {
+          return cat.image.url;
+        }
+      }),
     );
 }
 
@@ -18,17 +22,38 @@ async function showCats() {
 
   try {
     const cats = await getCatsBreedAndPhoto();
+    // console.log(cats);
     cats.forEach((cat) => {
-      const [cat_breed, cat_photo] = cat;
+      const { id, image, name, origin } = cat;
+      console.log(id, name, image, origin);
+      const a = document.createElement("a");
       const figure = document.createElement("figure");
       const img = document.createElement("img");
-      const figcaption = document.createElement("figure");
-      img.src = cat_photo;
-      figcaption.textContent = cat_breed;
-      figure.append(img);
-      figure.append(figcaption);
+      const div = document.createElement("div");
+      const h2 = document.createElement("h2");
+      const p = document.createElement("p");
 
-      grid.append(figure);
+      a.href = `#`;
+      a.classList.add("cat-item");
+
+      h2.classList.add("cat-item__breed");
+      h2.textContent = name;
+
+      p.classList.add("cat-item__origin");
+      p.textContent = origin;
+
+      div.classList.add("cat-item__header");
+      div.append(h2);
+      div.append(p);
+
+      img.src = image.url;
+      figure.classList.add("cat-item__thumbnail");
+      figure.append(img);
+      figure.append(div);
+
+      a.append(figure);
+
+      grid.append(a);
     });
   } catch (err) {
     console.log(err.message);
